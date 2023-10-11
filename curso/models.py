6fba36da6a9453ff013832751efbf21f73b62040
lsegecode <sanabrialucas97@gmail.com>
2023-10-11 20:25:20 -0300

    Funcionando todos los CRUD

--- curso/models.py
+++ curso/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from materia import models as materias_models
+
 
 # Create your models here.
 class Curso(models.Model):
     id_curso = models.AutoField(primary_key=True)
     Annio = models.CharField(max_length=5)
     Division = models.CharField(max_length=4)
+    id_materia = models.ForeignKey(materias_models.Materia, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         texto = "{0}, {1}"
